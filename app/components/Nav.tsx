04af76ee1f37b422c2afd193700f0b720447d09e
Trend20 <enockomondi305@gmail.com>
2023-10-17 14:33:37 +0300

    adding nav logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,11 +1,16 @@
+'use-client';
 import {navData} from "@/data/app_data";
 import Link from "next/link";
+import logoImg from '../../public/assets/images/contr.png'
+import Image from 'next/image';
 
 const Navbar = () =>{
     return(
         <div className='nav flex justify-between p-5 w-full items-center bg-green-500'>
-            <div className="logo w-20 flex justify-center">
-                <Link href={'/'}><h1>ContriQuest</h1></Link>
+            <div className="logo w-40 flex justify-center">
+                <Link href={'/'}>
+                    <Image src={logoImg} alt={"logo"}></Image>
+                </Link>
             </div>
             <div className="nav-links flex w-50 justify-between">
                 {
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className="h-full">
       <Hero />
-      {/* <Hero />
-      <Hero />
-      <Hero /> */}
     </main>
   )
 }
