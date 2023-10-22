b912db9d05ff7e5ffccc6fa11454c8babfd78d90
Trend20 <enockomondi305@gmail.com>
2023-10-22 15:48:21 +0300

    hero and navbar component fixes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,18 +2,18 @@
 import React from 'react'
 import Image from 'next/image';
 import heroImg from '../../public/assets/images/hero.jpeg'
-import testImg from '../../public/assets/images/test.png'
+// import testImg from '../../public/assets/images/test.png'
 import Link from 'next/link';
 import { AiFillStar} from 'react-icons/ai'
 
 const Hero = () => {
   return (
     <div className='w-full flex pt-40 justify-between'>
-        <div className="flex flex-col mt-6 px-16 w-1/2">
-          <h1 className='text-7xl font-black tracking-tight text-gray-900'>Rank Developers Based on Project Impact and Activity.</h1>
+        <div className="flex flex-col mt-12 px-16 w-1/2">
+          <h1 className='text-8xl font-black tracking-tight text-gray-900'>Rank Developers Based on Project Impact and Activity.</h1>
           <p className='prose prose-user mt-6 text-2xl text-gray-500 leading-8 w-3/4'>Join ContriQuest and see where you stand among the top developers in the industry.</p>
           <Link href='/waitlist' className='rounded w-32 bg-amber-700 font-bold flex justify-center items-center p-3 text-white mt-6 hover:bg-amber-800'>Join now</Link>
-          <div className="flex space-x-6 mt-5 divide-x-2 divide-gray-300 items-center">
+          <div className="flex space-x-6 mt-12 divide-x-2 divide-gray-300 items-center">
             <p className='flex'>
              <AiFillStar fill='#EAB308'/>
              <AiFillStar fill='#EAB308'/>
@@ -24,12 +24,12 @@ const Hero = () => {
             <p className='flex pl-3'>Transparent rankings</p>
             <p className='flex pl-3'>Showcase your skills</p>
           </div>
-          <span className='flex space-x-6items-center mt-5'>
+          {/* <span className='flex space-x-6 items-center mt-5'>
             <Image src={testImg} alt='test-image' className="h-10 w-10 rounded-full border p-1 border-yellow-700"/>
             <p className=' text-black ml-5 w-3/4'>
-              ContriQuest has helped me gain visibility and recognition in the developer community. It's a great platform to showcase my skills and connect with other talented developers.
+              ContriQuest has helped me gain visibility and recognition in the developer community. It is a great platform to showcase my skills and connect with other talented developers.
             </p>
-          </span>
+          </span> */}
         </div>
         <div className="flex max-w-4xl">
           <Image src={heroImg} alt='hero_img' className='rounded-l-lg'/>
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,40 +1,37 @@
 'use client';
 import { usePathname } from "next/navigation";
-import {navData} from "@/data/app_data";
+// import {navData} from "@/data/app_data";
 import Link from "next/link";
-import { TbMenuOrder} from 'react-icons/tb'
+import { GiUpgrade} from 'react-icons/gi'
 
 const Navbar = () =>{
     const currentRoute = usePathname();
     // console.log(currentRoute);
     const actionLinks = [
         {
-            linkName: 'Login',
+            linkName: 'Get Started',
             linkUrl:'/login'
         },
-        {
-            linkName: 'Register',
-            linkUrl:'/signup'
-        }
     ]
     return(
         <div className='flex justify-between p-5 w-full items-center'>
-            <div className="flex items-center space-x-6 w-60">
-                <Link href={'/'}>
-                   <TbMenuOrder size='50'/>
+            <div className="flex items-center space-x-6 w-60 px-16">
+                <Link href={'/'} className="flex items-center space-x-1">
+                   <GiUpgrade size='40' fill='#fff' className="flex items-center justify-center p-1 bg-black rounded-full"/>
+                   <p className="flex text-2xl font-bold">ContriQuest</p>
                 </Link>
-                <div className="flex">
+                {/* <div className="flex">
                     {
                     navData.map((item) =>(
                         <Link href={item.itemLink} key={item.id} className="flex px-5 py-0">{item.item}</Link>
                     ))
                     }
-                </div>
+                </div> */}
             </div>
             <div className="flex w-36 space-x-6 items-center">
                 {
                     actionLinks.map((link) =>(
-                        <Link key={link.linkUrl} href={link.linkUrl} className="flex w-48">{link.linkName}</Link>
+                        <Link key={link.linkUrl} href={link.linkUrl} className="flex w-48 justify-center items-center p-4 font-bold rounded-md bg-black text-white">{link.linkName}</Link>
                     ))
                 }
             </div>
